e274ee7397d9c8f957078a4be53d48132183634d
Aleksey Pirogov <astynax@users.noreply.github.com>
2018-09-24 20:54:18 +0300

    [art] Add "circuit board"

---
+++ source/2019-09-24-circuit-board.rkt
@@ -0,0 +1,29 @@
+#lang racket
+(require 2htdp/image)
+
+; partial application: (($ + 1 2) 3 4) => 10
+(define ($ f . args)
+  (lambda (a . args2)
+    (apply f (append args (cons a args2)))))
+
+(define (sq i f s)
+  (let ([i1 (i      f  s)]
+        [i2 (i (not f) s)])
+    (above (beside i1 i2)
+           (beside i2 i1))))
+
+(define (piece f s)
+  ((if f
+       flip-horizontal
+       ($ overlay
+          (circle (/ s 8) 'solid 'black)
+          (circle (/ s 4) 'solid 'gray)))
+   (line s s (make-pen 'gray (/ s 10) 'solid 'round 'round))))
+
+(save-image
+  (let ([i (sq ($ sq ($ sq ($ sq piece))) #f 40)])
+    (overlay i
+             (rectangle (image-width i)
+                        (image-height i) 'solid 'darkgreen)))
+  "output.png")
+
