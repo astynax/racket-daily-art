f3d2ee53898fb58f9af012e2f36d6d86c9abf1df
Aleksey Pirogov <astynax@users.noreply.github.com>
2018-09-28 23:44:45 +0300

    [art] add "cyan mesh"

---
+++ source/2019-09-28-cyan-mesh.rkt
@@ -0,0 +1,31 @@
+#lang racket
+(require 2htdp/image)
+(require racket/set)
+
+(define all9 (list->set (range 9)))
+
+(define (->color x)
+  (case x
+    [(4) 'DarkSlateBlue]
+    [(3) 'SteelBlue]
+    [(2) 'DarkTurquoise]
+    [(1) 'Cyan]
+    [else 'Yellow]))
+
+(define (step v c s)
+  (if (or (set-empty? v) (< s 10))
+      (circle s 'solid (->color c))
+      (let* ([s3 (/ s 3)]
+             [item (lambda (x)
+                     (if (set-member? v x)
+                         (step (set-remove v x) (+ c 1) s3)
+                         (step (set) c s3)))])
+        (above (beside (item 8) (item 7) (item 6))
+               (beside (item 5) (item 4) (item 3))
+               (beside (item 2) (item 1) (item 0))))))
+
+(save-image
+ (overlay
+  (step all9 0 400)
+  (square 800 'solid 'black))
+ "output.png")
