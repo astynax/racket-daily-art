a77bf2fe446dbe0f20dc7c140407b935539e5d97
Aleksey Pirogov <astynax@users.noreply.github.com>
2018-10-02 21:34:37 +0300

    [art] add "cluster"

---
+++ source/2019-10-02-cluster.rkt
@@ -0,0 +1,24 @@
+#lang racket
+(require 2htdp/image)
+
+(define (step i n s)
+  (if (< s 20)
+      (i n s)
+      (let* ([s3 (/ s 3)]
+             [ii (i n s3)]
+             [si (step i (+ n 1) s3)]
+             [bi (rotate 180 (step i (+ n 1) (* s3 2)))])
+        (above (beside (above ii si) bi)
+               (beside si ii si)))))
+
+(define (item n s)
+  (overlay
+   (circle (/ s 20) (if (zero? (modulo n 2)) 200 0) 'Cyan)
+   (circle (/ s 3) 'solid 'black)
+   (square s (max 0 (- 220 (* n 20))) 'SkyBlue)))
+
+(save-image
+ (overlay
+  (step item 0 600)
+  (square 600 'solid 'black))
+ "output.png")
