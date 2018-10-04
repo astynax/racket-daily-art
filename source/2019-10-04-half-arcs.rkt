e2a476183b284e6b69ef34f2259185b2d7e8a173
Aleksey Pirogov <astynax@users.noreply.github.com>
2018-10-05 00:12:50 +0300

    [art] add "half arcs"

---
+++ source/2019-10-04-half-arcs.rkt
@@ -0,0 +1,26 @@
+#lang racket
+(require 2htdp/image)
+
+(define (item s)
+  (let ([dot (circle (/ s 4) 'solid 'teal)])
+    (overlay (beside (overlay dot (circle (/ s 2) 'solid 'gold))
+                     (overlay dot (circle (/ s 2) 'solid 'white)))
+             (rectangle (* 2 s) s 'solid 'teal))))
+
+(define (step i s)
+  (if (< s 20)
+      (i s)
+      (let ([ii (step i (/ s 2))])
+        (overlay/align
+         'left 'center
+         (rotate 90 ii)
+         (overlay/align
+          'right 'center    
+          (rotate -90 ii)
+          (i s))))))
+
+(save-image
+ (let* ([i (step item 400)]
+        [r (rotate 90 i)])
+   (overlay i (beside r r)))
+ "output.png")
